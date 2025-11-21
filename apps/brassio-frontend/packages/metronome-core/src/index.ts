0f1cbaee67218384a5607c86e1a41111e17f912e
PixelCraftIO <d.fiebich@outlook.de>
2025-11-21 07:17:16 +0000

    234234

--- apps/brassio-frontend/packages/metronome-core/src/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './types'
-export * from './constants'
-export * from './metronome-engine'
-export * from './rhythm-sequence-engine'
--- apps/brassio-frontend/packages/metronome-ui/src/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './hooks/use-metronome'
-export * from './hooks/use-rhythm-sequence'
